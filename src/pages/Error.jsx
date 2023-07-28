dff72926116b81eada1d6d3c82d0d1048362ea6b
Shivansh876 <shivanshmahajan876@gmail.com>
2023-07-28 11:12:03 +0530

    Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import ErrorImage from "../assets/Images/Error.svg"
 
 const Error = () => {
   return (
-    <div className='justify-center items-center text-3xl text-richblack-50'>
-        Error 404, not found 
+    <div className='flex flex-col justify-center items-center m-12'>
+      <div className='justify-center items-center text-3xl text-richblack-50'>
+        Error 404, not found
+      </div>
+      
+      <div className='w-[600px] h-[600px] lg:p-[10px] p-[100px]'>
+        <img src={ErrorImage} alt="image here" />
+      </div>
     </div>
   )
 }
